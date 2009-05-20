b549abeea7092e444fddb1604c8dcd68428926ac
Ari Lerner <arilerner@mac.com>
2009-05-19 22:51:15 -0700

    Added file loader to load files with server setups

--- lib/troph/messaging/handlers/presence_handler.rb
+++ lib/troph/messaging/handlers/presence_handler.rb
@@ -1,5 +1,6 @@
 module Troph
   class PresenceHandler < BaseHandler
+    
     queue_name Troph::QUEUES[:presence]
     
     def handle_message msg
@@ -9,7 +10,7 @@ module Troph
         puts "Still responding. Send a ping back"        
       else
         puts "Received presence message: #{msg}"
-      end      
+      end
     end
     
   end
